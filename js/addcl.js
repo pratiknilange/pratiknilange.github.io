c0705d044cdca3ca857ebfc289ae67acc1e7f093
PratikNilange <PratikNilange@users.noreply.github.com>
2021-09-04 12:08:16 +0530

    me 1208;

--- js/addcl.js
+++ js/addcl.js
@@ -1,4 +1,4 @@
 // Add class JS
 
-$(".pn-page").addClass("container col-lg-6 mx-auto py-4");
+$(".pn-page").addClass("container col-lg-6 mx-auto py-4 px-3");
 $(".pn-content").addClass("col-lg-60");
